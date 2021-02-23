3f675b87dfb6e4ebe95b618e66742c5d584f29ad
mike-petrov <mis5116@yandex.ru>
2021-02-23 19:22:54 +0300

    add /home & /category & /search

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify, make_response
 from pymongo import MongoClient
 
 import sys
@@ -19,18 +19,51 @@ link = link.format("Leonid", mongo_pass, mongo_db)
 client = MongoClient(link, connect=False)
 db = client[config.mongo_db_name]
 
+@app.route('/home', methods=['GET'])
+def get_home():
+    try:
+        # https://romanychev.online/nethub/home
+        groups_data = db.groups.find({}, {'_id': False})
+        groups = []
+        for group in groups_data:
+            groups.append(group)
 
-@app.route('/rooms', methods=['GET'])
-def get_rooms():
-    '''
-    users_ids = []
-    users = db.groups.find()
-    for user in users:
-        users_ids.append(int(user['vk_id']))
-    '''
+        categories = {}
+        for group in groups:
+            categories[group['category']] = [group]
 
-    return "hello"
+        return jsonify({'top': groups, 'categories': categories})
+    except:
+        return jsonify({'server': 'error'}), 400
 
+@app.route('/category', methods=['GET'])
+def get_category():
+    try:
+        # https://romanychev.online/nethub/category?id=3&amount=10
+        category_id = int(request.args.get('id'))
+        category_amount = int(request.args.get('amount'))
+        groups_data = db.groups.find({'category': category_id }, {'_id': False}).limit(category_amount)
+        category = []
+        for group in groups_data:
+            category.append(group)
+
+        return jsonify(category)
+    except:
+        return jsonify({'server': 'error'}), 400
+
+@app.route('/search', methods=['GET'])
+def search():
+    try:
+        # https://romanychev.online/nethub/search?text=Кто
+        text = request.args.get('text')
+        groups_data = db.groups.find({'title': {'$regex': text}}, {'_id': False})
+        category = []
+        for group in groups_data:
+            category.append(group)
+
+        return jsonify(category)
+    except:
+        return jsonify({'server': 'error'}), 400
 
 if __name__ == '__main__':
     app.run(
